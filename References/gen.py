c33668229456f113efaae17bcc9949d0f35e5dbe
DisabledMallis <mallisiscool@gmail.com>
2021-01-31 00:37:31 -0500

    no buildy because ref dumb

--- References/gen.py
+++ References/gen.py
@@ -2,4 +2,4 @@ import os
 for filename in os.listdir():
     if(filename.endswith(".dll")):
         goodName = filename.replace(".dll", "");
-        print("""set_property(TARGET ${project} PROPERTY VS_DOTNET_REFERENCES_"""+goodName+""" "${CMAKE_CURRENT_LIST_DIR}/"""+goodName+""".dll")""")
+        print("""set_property(TARGET NKHook6 PROPERTY VS_DOTNET_REFERENCES_"""+goodName+""" "${CMAKE_CURRENT_LIST_DIR}/"""+goodName+""".dll")""")
