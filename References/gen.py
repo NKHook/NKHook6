a38cca5a2a3dae379b5ac4eeeba32967d096e891
DisabledMallis <mallisiscool@gmail.com>
2021-02-02 00:30:47 -0500

    Wait bruh :facepalm:

--- References/gen.py
+++ References/gen.py
@@ -3,5 +3,5 @@ print("set_property(TARGET NKHook6 PROPERTY VS_DOTNET_REFERENCES")
 for filename in os.listdir():
     if(filename.endswith(".dll")):
         goodName = filename.replace(".dll", "");
-        print("""\t"""+goodName+""" ${CMAKE_CURRENT_LIST_DIR}/"""+goodName+""".dll;""")
+        print("""\t${CMAKE_CURRENT_LIST_DIR}/"""+goodName+""".dll;""")
 print(")")
