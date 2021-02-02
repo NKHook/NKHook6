ee2800b9303ddec2069f4b7521aef8032edd494a
DisabledMallis <mallisiscool@gmail.com>
2021-02-02 00:28:26 -0500

    closer to a working cmakelists

--- References/gen.py
+++ References/gen.py
@@ -1,7 +1,7 @@
 import os
-print("set_property(TARGET NKHook6 PROPERTY VS_DOTNET_REFERENCES \"");
+print("set_property(TARGET NKHook6 PROPERTY VS_DOTNET_REFERENCES")
 for filename in os.listdir():
     if(filename.endswith(".dll")):
         goodName = filename.replace(".dll", "");
-        print("""\t"""+goodName+";")
-print("\")");
+        print("""\t"""+goodName+""" ${CMAKE_CURRENT_LIST_DIR}/"""+goodName+""".dll;""")
+print(")")
