acaa3637efbd58c259c7384fdd8a354108128ac1
DisabledMallis <mallisiscool@gmail.com>
2021-02-03 13:32:55 -0500

    Working sell patch

---
+++ NKHook6-Impl/Event-Patches/Towers/SoldPatch.cs
@@ -0,0 +1,22 @@
+using Assets.Scripts.Simulation;
+using Assets.Scripts.Simulation.Towers;
+using Harmony;
+using NKHook6.API.Events;
+using NKHook6.API.Events._Towers;
+using NKHook6_Impl.Implementations.Towers;
+
+namespace NKHook6_Impl.Towers
+{
+    [HarmonyPatch(typeof(Simulation), nameof(Simulation.SellTower))]
+    internal class SoldPatch
+    {
+        [HarmonyPrefix]
+        internal static bool Prefix(Simulation __instance, Tower tower, int inputId)
+        {
+            NTowerEntity towerEntity = new NTowerEntity(tower);
+            var o = new TowerEvents.SoldEvent(towerEntity, tower.SaleValue); //Create SoldPatch instance
+            EventRegistry.instance.dispatchEvent(ref o); //Dispatch it
+            return !o.isCancelled();
+        }
+    }
+}
