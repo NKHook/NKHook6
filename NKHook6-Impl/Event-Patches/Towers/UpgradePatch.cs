442503201d9471ab55c8e7e7ba3030d84326cc15
DisabledMallis <mallisiscool@gmail.com>
2021-02-03 14:21:45 -0500

    Working upgrade cancelling

---
+++ NKHook6-Impl/Event-Patches/Towers/UpgradePatch.cs
@@ -0,0 +1,23 @@
+using Assets.Scripts.Simulation;
+using Assets.Scripts.Simulation.Towers;
+using Assets.Scripts.Unity.Bridge;
+using Harmony;
+using NKHook6.API.Events;
+using NKHook6.API.Events._Towers;
+using NKHook6_Impl.Implementations.Towers;
+
+namespace NKHook6_Impl.Towers
+{
+    [HarmonyPatch(typeof(TowerToSimulation), nameof(TowerToSimulation.Upgrade))]
+    internal class UpgradePatch
+    {
+        [HarmonyPrefix]
+        internal static bool Prefix(TowerToSimulation __instance, int pathIndex, Il2CppSystem.Action<bool> callback)
+        {
+            NTowerEntity towerEntity = new NTowerEntity(__instance.tower);
+            var o = new TowerEvents.UpgradeEvent(towerEntity); //Create UpgradePatch instance
+            EventRegistry.instance.dispatchEvent(ref o); //Dispatch it
+            return !o.isCancelled();
+        }
+    }
+}
