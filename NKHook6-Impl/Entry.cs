1c064d25748bd64a2ea28e7b699d6ca35e92865b
DisabledMallis <mallisiscool@gmail.com>
2021-02-02 19:53:01 -0500

    Remove test stuff

--- NKHook6-Impl/Entry.cs
+++ NKHook6-Impl/Entry.cs
@@ -23,52 +23,8 @@ namespace NKHook6_Impl
             Logger.Log("Google translate of NKHook6 API to TARGET has begun".Replace("TARGET", "BloonsTD6"));
         }
 
-        [EventAttribute("BloonCreatedEvent")]
-        public static void onCreated(BloonEvents.CreatedEvent e)
-        {
-            Logger.Log("Bloon created event");
-            IBloon bloonType = e.getBloon().getType();
-            if (bloonType is RedBloon)
-            {
-                RedBloon newType = (RedBloon)bloonType;
-                Logger.Log("The bloon was a red bloon");
-            }
-        }
-        
-        [EventAttribute("BloonLeakedEvent")]
-        public static void onLeaked(BloonEvents.LeakedEvent e)
-        {
-            Logger.Log("Bloon leaked event");
-        }
-        
-        [EventAttribute("BloonMoveEvent")]
-        public static void onMove(BloonEvents.MoveEvent e)
-        {
-            IBloonEntity bloon = e.getBloon();
-            //Logger.Log("Progress: "+bloon.getProgress());
-            float[] pos = bloon.getPosition();
-            //Logger.Log("Position (X,Y,Z)".Replace("X", pos[0].ToString()).Replace("Y", pos[1].ToString()).Replace("Z", pos[2].ToString()));
-            if (bloon.getProgress() > 500)
-            {
-                e.SetCancelled(true);
-            }
-        }
-
-        [EventAttribute("BloonRotateEvent")]
-        public static void onRotate(BloonEvents.RotateEvent e)
-        {
-            IBloonEntity bloon = e.getBloon();
-            Logger.Log(bloon.getRotation().ToString());
-        }
-
         public void onModLoaded()
         {
-            Game game = Game.instance;
-            List<BloonModel> models = game.getAllBloonModels();
-            foreach (var model in models)
-            {
-                Logger.Log(model.name);
-            }
             EventRegistry.instance.listen(typeof(Entry));
         }
     }
