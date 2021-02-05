ba39820da953842b0337ed349e56f4f5cffefdd9
DisabledMallis <mallisiscool@gmail.com>
2021-02-04 20:45:55 -0500

    Add YellowBloon

---
+++ NKHook6-Impl/Implementations/Bloons/YellowBloon.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Scripts.Models.Bloons;
+using Assets.Scripts.Unity;
+using NKHook6_Impl.Extensions;
+
+namespace NKHook6_Impl.Implementations.Bloons
+{
+    public class YellowBloon : NBloon
+    {
+        public YellowBloon() : base(Game.instance.getBloonModel("Yellow"))
+        {
+        }
+        internal YellowBloon(string modelName) : base(Game.instance.getBloonModel(modelName))
+        {
+        }
+    }
+}
---
+++ NKHook6-Impl/Implementations/Bloons/YellowCamoBloon.cs
@@ -0,0 +1,13 @@
+﻿using Assets.Scripts.Models.Bloons;
+using Assets.Scripts.Unity;
+using NKHook6_Impl.Extensions;
+
+namespace NKHook6_Impl.Implementations.Bloons
+{
+    public class YellowCamoBloon : YellowBloon
+    {
+        public YellowCamoBloon() : base("YellowCamoBloon")
+        {
+        }
+    }
+}
