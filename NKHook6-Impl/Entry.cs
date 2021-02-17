a967163489c384523dfa030be79f298ede5c0172
DisabledMallis <mallisiscool@gmail.com>
2021-02-17 13:45:16 -0500

    Remove test code

--- NKHook6-Impl/Entry.cs
+++ NKHook6-Impl/Entry.cs
@@ -1,12 +1,6 @@
-﻿using Assets.Scripts.Unity;
-using MelonLoader;
+﻿using MelonLoader;
 using NKHook6.API;
-using NKHook6.API.Bloons;
 using NKHook6.API.Events;
-using NKHook6.API.Events._Bloons;
-using NKHook6_Impl.Implementations.Bloons;
-using NKHook6.API.Registry;
-using NKHook6.API.Events._Game;
 
 namespace NKHook6_Impl
 {
@@ -20,12 +14,6 @@ namespace NKHook6_Impl
             new EventRegistry(); //Initialize EventRegistry
 
             Logger.Log("Google translate of NKHook6 API to TARGET has begun".Replace("TARGET", "BloonsTD6"));
-        }
-
-        [EventAttribute("RoundStartEvent")]
-        public static void onRound(GameEvents.RoundStartEvent e)
-        {
-            Logger.Log("Round: "+e.round);
         }
 
         public void OnModLoaded()
